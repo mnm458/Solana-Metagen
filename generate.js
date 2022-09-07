469973024336595e91c6a3f7924bcdcc7751dd54
mnm458 <mohaknainmalhotra@gmail.com>
2022-09-07 12:28:45 +1000

    creators block

--- generate.js
+++ generate.js
@@ -11,3 +11,17 @@ const name = "Your collection name";
 const symbol = "Your collection symbol";
 const description = "Your collection description";
 const fee_basis = 1000; // Define how much % you want from secondary market sales 1000 = 10%
+
+const creators = [
+    //Creator 1 Example
+    {
+      "address": "0x1Example",
+      "share": 50
+    },
+    //Creator 2 Example
+    {
+      "address": "0x2Example",
+      "share": 50
+    },
+  ]
+  
